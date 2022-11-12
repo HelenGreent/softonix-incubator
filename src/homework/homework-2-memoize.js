a9ac3a92f073e294bb3515e02f0a3bf807aa6a72
pro <pro@pros-MacBook-Pro.local>
2022-11-12 23:12:28 +0200

    Add hw-2

--- src/homework/homework-2-memoize.js
+++ src/homework/homework-2-memoize.js
@@ -4,13 +4,23 @@
 function sum (a, b) { return a + b } // for test
 
 // eslint-disable-next-line
-function memoize (fn) {
-  // fn ваш код тут...
+function memoize(fn) {
+	// fn ваш код тут...
+	const cache = new Map();
+	return (...arg) => {
+		const key = JSON.stringify(arg);
+		if (cache.has(key)) {
+			return cache.get(key);
+		}
+		const result = fn(...arg);
+		cache.set(key, result);
+		return result;
+	};
 }
 
 // приклад виконання вашого коду
 const sumMemoized = memoize(sum)
 
-sumMemoized(1, 3) // результат 4
-sumMemoized(3, 3) // результат 6
-sumMemoized(1, 3) // результат 4, відбулось повторне виконання, результат повернуто з кешу без виклику додавання
+console.log(sumMemoized(1, 3)) // результат 4
+console.log(sumMemoized(3, 3)) // результат 6
+console.log(sumMemoized(1, 3)) // результат 4, відбулось повторне виконання, результат повернуто з кешу без виклику додавання
