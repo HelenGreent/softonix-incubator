1a67295b3cf10cccbfe1ef7a34786cd2b7e22115
Helen <helengreent23@gmail.com>
2022-12-08 13:41:58 +0200

    Update branch

--- dts/global.d.ts
+++ dts/global.d.ts
@@ -1,25 +1,22 @@
 import 'vue-router'
-import { routeNames } from '@/router'
-import { EPortalNames } from '@/plugins'
-import { EComponentSize, EComponentType } from '@/types'
+import { routeNames } from '@/router/route-names'
+import { globalProperties } from '@/plugins'
+import type { EpPropFinalized, EpPropMergeType } from 'element-plus/es/utils'
 
 declare module 'vue-router' {
   interface RouteMeta {
     // todo: this is just an example. Please setup your own route meta params.
-    label?: string;
-    parentName?: string
-    requireAuth?: boolean
   }
 }
 
 declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
+  interface ComponentCustomProperties {
     // todo: Here you define you global vue definitions.
-    $portalNames: typeof EPortalNames
     $routeNames: typeof routeNames
 
-    // todo: These ones are used only for element library for size and type props;
-    $componentSize: typeof EComponentSize
-    $componentType: typeof EComponentType
+    $elComponentSize: Record<keyof typeof globalProperties.$elComponentSize, EpPropMergeType<StringConstructor, keyof typeof globalProperties.$elComponentSize>>
+    $elComponentType: Record<keyof typeof globalProperties.$elComponentType, EpPropFinalized<StringConstructor, keyof typeof globalProperties.$elComponentType>>
   }
 }
+
+export { }
